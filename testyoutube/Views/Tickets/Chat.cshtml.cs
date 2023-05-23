5175a468e7b2ed111139bfc0526ce4cd7a52963f
Roman Germain <roman.germain@ymail.com>
2023-05-23 15:56:32 +0200

    correction bug chat + responsive des pages

--- testyoutube/Views/Tickets/Chat.cshtml.cs
+++ testyoutube/Views/Tickets/Chat.cshtml.cs
@@ -1,1 +1,14 @@
-﻿
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+
+public class InputModel : PageModel
+{
+
+    [BindProperty]
+    public InputModel Input { get; set; }
+
+    [Required]
+    [StringLength(500, ErrorMessage = "Le message ne peut pas dépasser 500 caractères.")]
+    public string Message { get; set; }
+}
